633403a28936ae13a0dd8db56fd742f05a00fbed
unknown <ivan.ruskevych@gmail.com>
2023-05-14 22:35:39 +0300

    step-05 state-machine-->done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,42 +7,46 @@ import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 export default class App extends Component {
   state = {
     images: [],
-    text: '',
+    query: '',
     status: 'idle',
   };
 
   componentDidMount = () => {
-    if (this.state.text === '') {
+    if (this.state.query === '') {
       return this.setState({ status: 'idle' });
     }
-
-    imagesApi
-      .fetchImages()
-      .then(response => {
-        this.setState({ images: response.hits });
-        this.setState({ status: 'resolved' });
-      })
-      .catch(err => {
-        this.setState({ status: 'rejected' });
-      });
   };
 
-  handleSearchText = text => {
-    console.log('handleSearchText', text);
-    return this.setState({ text });
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.query !== this.state.query) {
+      imagesApi
+        .fetchImages(this.state.query)
+        .then(response => {
+          this.setState({ images: response.hits });
+          this.setState({ status: 'resolved' });
+        })
+        .catch(err => {
+          this.setState({ status: 'rejected' });
+        });
+    }
+  };
+
+  handleFormSubmit = query => {
+    // console.log('handleFormSubmit', query);
+    return this.setState({ query });
   };
 
   render() {
     const { images, status } = this.state;
 
     if (status === 'idle') {
-      return <Searchbar onSubmit={this.handleSearchText} />;
+      return <Searchbar onSubmit={this.handleFormSubmit} />;
     }
 
     if (status === 'resolved') {
       return (
         <div>
-          <Searchbar onSubmit={this.handleSearchText} />
+          <Searchbar onSubmit={this.handleFormSubmit} />
           <ImageGallery>
             <ImageGalleryItem images={images}></ImageGalleryItem>
           </ImageGallery>
