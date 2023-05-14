39848c9699e3a9113cf1af2c9d9c76ab91900044
unknown <ivan.ruskevych@gmail.com>
2023-05-14 23:07:57 +0300

    step-06 loader-->done

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,1 +1,18 @@
-console.log('Loader');
+import { Circles } from 'react-loader-spinner';
+import css from './Loader.module.css';
+export default function Loader() {
+  return (
+    <div className={css.Loader}>
+      <Circles
+        height="180"
+        width="180"
+        color="#4fa94d"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+      ;
+    </div>
+  );
+}
